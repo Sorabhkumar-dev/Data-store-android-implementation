37ce5853819e052c629f33363b32925eb1c41628
Sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-11-04 11:20:43 +0530

    chore: first commit

---
+++ app/src/main/java/com/sorabh/datastore/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sorabh.datastore.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sorabh.datastore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/sorabh/datastore/ui/fragment/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.sorabh.datastore.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sorabh.datastore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding:FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+
+
+        return binding.root
+    }
+}
