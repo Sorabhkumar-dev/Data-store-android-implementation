ea51f3fd6d836c4bfb3674419b02f8e2a58d5688
Sorabh kumar <sorabhkumardeveloper@gmail.com>
2022-11-08 00:23:49 +0530

    chore:completed ui 100 %

--- app/src/main/java/com/sorabh/datastore/ui/fragment/SendMoneyFragment.kt
+++ app/src/main/java/com/sorabh/datastore/ui/fragment/SendMoneyFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.sorabh.datastore.R
 import com.sorabh.datastore.databinding.FragmentSendMoneyBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +21,21 @@ class SendMoneyFragment : Fragment() {
     ): View {
         binding = FragmentSendMoneyBinding.inflate(inflater)
 
+        binding.btnSendMoney.setOnClickListener {
+
+        }
         return binding.root
+    }
+
+    private fun showDialog(receiverName:String){
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+            .setIcon(R.drawable.ic_logo)
+            .setTitle(getString(R.string.send_money_to,receiverName))
+            .setMessage(getString(R.string.payment_successfully_completed_to,receiverName))
+            .setPositiveButton(getString(R.string.ok)){ dialog, _ ->
+                dialog.dismiss()
+
+            }.create()
+        dialog.show()
     }
 }
